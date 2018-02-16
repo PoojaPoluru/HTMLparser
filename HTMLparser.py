aa8b7fbad7ec31c47413c97b0bcae72af8123ea3
PoojaPoluru <ppoluru@uncc.edu>
2018-02-15 19:04:09 -0800

    Task1 : Getting the names of the authors

--- HTMLparser.py
+++ HTMLparser.py
@@ -1,2 +1,15 @@
 from bs4 import BeautifulSoup
 import glob
+
+path='C:/Users/Pooja/Desktop/lot-parser/data/2015-03-18/*.html'   #path to the folder
+files=glob.glob(path)
+arr=[] #initializing empty array to store the objects
+
+for file in files:
+    #links = urlopen(file).read()
+    links = open(file, 'r')             #opening each file and reading it
+    BS=BeautifulSoup(links,"lxml")
+    artist=BS.h2.contents[0].strip()    #getting the contents of h2 tag for the artist name
+    while(artist not in arr):
+        arr.append(artist)
+print(arr)
