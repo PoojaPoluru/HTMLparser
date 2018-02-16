545bde1fa4df141f1e73df72e6de5740a1924938
PoojaPoluru <ppoluru@uncc.edu>
2018-02-15 19:16:18 -0800

    Task3 : Getting the names of the authors and work and the price of paintings as json objects

--- HTMLparser.py
+++ HTMLparser.py
@@ -6,14 +6,16 @@ path='C:/Users/Pooja/Desktop/lot-parser/data/2015-03-18/*.html'   #path to the f
 files=glob.glob(path)
 arr=[]                                                             #initializing empty array to store the objects
 new_dict={}                                                         #initializing a new dictionary
-
+sub_dict={}
 for file in files:
     #links = urlopen(file).read()
     links = open(file, 'r')             #opening each file and reading it
     BS=BeautifulSoup(links,"lxml")
     artist=BS.h2.contents[0].strip()                                  #getting the contents of h2 tag for the artist name
     title = BS.title.contents[0].strip()                              #obtainign the title
-    new_dict={"artist":artist,"works":title}                           #new dictionary using json object format
+    price = BS.find_all("div")[1].string                               #getting the price as string
+    sub_dict={"title":title,"price":price}                             #creating a sub dictionary
+    new_dict = {"artist": artist, "works": [sub_dict]}
     arr.append(new_dict)                                               #array full
     json_obj=json.dumps(arr)                                            #getting the json format from the array
 print(json_obj)
